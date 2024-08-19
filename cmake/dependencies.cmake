2e301657743c2b3fc6c1d6baac3283e322b8a0eb
lukaskaz <lukask84@o2.pl>
2024-08-19 21:51:50 +0200

    Moving lidar code to external library, update (#13)
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -27,6 +27,28 @@ EXTERNALPROJECT_ADD(
 include_directories(${source_dir}/inc)
 link_directories(${build_dir}/build)
 
+set(source_dir "${CMAKE_BINARY_DIR}/libserial-src")
+set(build_dir "${CMAKE_BINARY_DIR}/libserial-build")
+
+EXTERNALPROJECT_ADD(
+  libserial
+  GIT_REPOSITORY    https://github.com/lukaskaz/lib-serial.git
+  GIT_TAG           main
+  PATCH_COMMAND     ${patching_cmd}
+  PREFIX            libserial-workspace
+  SOURCE_DIR        ${source_dir}
+  BINARY_DIR        ${build_dir}
+  CONFIGURE_COMMAND mkdir /${build_dir}/build &> /dev/null
+  BUILD_COMMAND     cd ${build_dir}/build && cmake -D BUILD_SHARED_LIBS=ON
+                    ${source_dir} && make -j 4
+  UPDATE_COMMAND    ""
+  INSTALL_COMMAND   ""
+  TEST_COMMAND      ""
+)
+
+include_directories(${source_dir}/inc)
+link_directories(${build_dir}/build)
+
 set(source_dir "${CMAKE_BINARY_DIR}/libclimenu-src")
 set(build_dir "${CMAKE_BINARY_DIR}/libclimenu-build")
 
